40378ebf180cd123d7da80f4c708be7fa3b0fb6e
JUNSEOK <kd02109@gmail.com>
2023-05-12 16:24:58 +0900

    [feat]: GlobalStyle, themeprovider 설정

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routers/Routers.jsx";
+import GlobalStyle from "./GlobalStyle.jsx";
+import { ThemeProvider } from "styled-components";
+import theme from "./theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+  font: {
+    large: "32px",
+    medium: "24px",
+    small: "16px",
+    footer: "12px",
+  },
+  title: "40px",
+  percentColor: "#452CDD",
+  footerColor: "#888888",
+};
+
+export default theme;
